a601209266fafde826d33e12132c66c422ec61a5
nkg1801 <84229588+nkg1801@users.noreply.github.com>
2021-05-31 21:27:10 +0530

    Update circularprogressindicator.dart

--- circularprogressindicator.dart
+++ circularprogressindicator.dart
@@ -42,8 +42,6 @@ class _MyHomePageState extends State<MyHomePage> {
               children: [
             _defaultSizeCircularIndicator(),
             _customSizeCircularProgressIndicator(),
-            _customSizeCircularProgressIndicator2(),
-            _customSizeCircularProgressIndicator3(),
           ])),
     );
   }
@@ -67,27 +65,6 @@ class _MyHomePageState extends State<MyHomePage> {
             child: CircularProgressIndicator(
               backgroundColor: Colors.redAccent,
               valueColor: AlwaysStoppedAnimation(Colors.green),
-              strokeWidth: 5,
-            )));
-  }
-
-  Widget _customSizeCircularProgressIndicator2() {
-    return Container(
-        margin: const EdgeInsets.all(20.0),
-        child: const LinearProgressIndicator());
-  }
-
-  Widget _customSizeCircularProgressIndicator3() {
-    return Container(
-        margin: const EdgeInsets.all(20.0),
-        child: const SizedBox(
-            width: 100.0,
-            height: 100.0,
-            child: CircularProgressIndicator(
-              backgroundColor: Colors.grey,
-              value: 0.8,
-              semanticsValue: 'XYZ',
-              semanticsLabel: '10.0',
               strokeWidth: 5,
             )));
   }
