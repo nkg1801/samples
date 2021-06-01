20bc23578787e73d0b23edbd6542c7c73e91bcaf
nkg1801 <84229588+nkg1801@users.noreply.github.com>
2021-06-01 21:40:18 +0530

    Create appbar.dart

---
+++ flutter/appbar.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: MyHomePage(title: 'My First Flutter app'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('My flutter app'),
+          actions: [
+            Text('Button 1'),   // you can try replacing Text widget with some other widgets like ElevatedButton (more about this later)
+            Text('Button 2'),
+            Text('Button 3'),
+          ],
+        ),
+        body: Text('Demonstration of AppBar action buttons')
+        );
+  }
+}
