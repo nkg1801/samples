e2cf37ea4253523dbb5767b553da3fd8fecee48c
nkg1801 <84229588+nkg1801@users.noreply.github.com>
2021-06-01 21:46:13 +0530

    Create center.dart

---
+++ flutter/center.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: MyHomePage(title: 'Center widget demo'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text(widget.title)),
+        body:
+          Container(
+              width: 200,
+              height:200,
+              color: Colors.grey,
+            child:
+              Center(
+                child:
+                  FlutterLogo(
+                    size: 100,),
+              )
+          )
+    );
+  }
+}
