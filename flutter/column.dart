ebf09761ad855bfd53b5a53cdb5e0f71c42d1ff2
nkg1801 <84229588+nkg1801@users.noreply.github.com>
2021-06-01 21:48:05 +0530

    Create column.dart

---
+++ flutter/column.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: MyHomePage(title: 'My First Flutter app'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(widget.title)),
+      body:
+        Column(
+          
+          children: [
+            //1st Child
+            SizedBox(width: 100.0, height: 100.0,
+              child:
+                DecoratedBox(decoration: BoxDecoration(color: Colors.red,)),
+            ),
+
+            <mark>// 2nd Child - this invisible SizedBox and being used here just for vertical spacing.</mark>
+            <mark>// Remember a SizedBox without any child is invisible but it still occupies space.</mark>
+            
+            SizedBox(height: 10,),
+
+            //3rd Child
+            SizedBox(width: 100.0, height: 100.0,
+              child:
+                 DecoratedBox(decoration: BoxDecoration(color: Colors.green,))
+            ),
+
+            <mark>// 4th Child - this invisible SizedBox and being used here just for vertical spacing.</mark>
+            <mark>// Remember a SizedBox without any child is invisible but it still occupies space.</mark>
+            
+            SizedBox(height: 10,),
+
+            //5th Child
+            SizedBox(width: 100.0, height: 100.0,
+                child:
+                DecoratedBox(decoration: BoxDecoration(color: Colors.blue,))
+            ),
+          ],
+        )
+    );
+  }
+}
