228e38d53e923ebcf9302b1b78ad1ab30005bd18
nkg1801 <84229588+nkg1801@users.noreply.github.com>
2021-06-01 21:49:34 +0530

    Create container.dart

---
+++ flutter/container.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: <mark>Container</mark>(
+            margin: const EdgeInsets.all(50.0),
+            color: Colors.grey,
+            width: 220.0,
+            height: 220.0,
+            child: const <mark>Align</mark>(
+                alignment: Alignment.center,
+                child: <mark>Text</mark>('Text Widget inside Container'))));
+  }
+}
