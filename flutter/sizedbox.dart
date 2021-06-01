1e2eafdf2b3d3246405da45aa826cf940b711ecc
nkg1801 <84229588+nkg1801@users.noreply.github.com>
2021-06-01 22:28:02 +0530

    Create sizedbox.dart

---
+++ flutter/sizedbox.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: MyHomePage(title: 'My First Flutter app'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('SizedBox demo')),
+      body:
+        <mark>SizedBox(width: 100.0, height: 100.0,
+          child: DecoratedBox(decoration: BoxDecoration(color: Colors.red))</mark>
+    ),
+    );
+  }
+}
