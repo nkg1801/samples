840165114e72dea74dad0d89b325d7735bf2eb3c
nkg1801 <84229588+nkg1801@users.noreply.github.com>
2021-06-02 17:48:59 +0530

    Create gesturedetector.dart

---
+++ flutter/gesturedetector.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() =&gt; _MyHomePageState();
+}
+
+class _MyHomePageState extends State&lt;MyHomePage&gt; {
+  int _counter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text('You clicked: $_counter times'),
+            GestureDetector(
+                onTap: () {
+                  setState(() {
+                    _counter++;
+                  });
+                },
+                child: const Text(
+                  'Click here to go to the next page.',
+                )),
+          ],
+        ),
+      ),
+    );
+  }
+}
